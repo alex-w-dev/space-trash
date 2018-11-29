edc1065c977272ed2977fe77c839c797812697cb
alex-w-dev <yampi.ru@gmail.com>
2018-11-29 15:06:24 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Space Trash
+
+Game
+
+## install
+
+1. npm install
+2. npm start
+
+## contacts
+
+Alexander [yampi.ru@gmail.com](mailto:yampi.ru@gmail.com)
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/js/index.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                modules: false
+              }],
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          }
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: [{
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              url: false
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: () => [
+                require('cssnano')({
+                  preset: ['default', {
+                    discardComments: {
+                      removeAll: true,
+                    },
+                  }]
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src/html/includes'),
+        use: ['raw-loader']
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/style.bundle.css"
+    }),
+    new CopyWebpackPlugin([{
+        from: './src/index.html',
+        to: './index.html'
+      },
+      {
+        from: './src/favicon',
+        to: './favicon'
+      },
+      {
+        from: './src/img',
+        to: './img'
+      },
+      {
+        from: './src/sounds',
+        to: './sounds'
+      },
+    ]),
+  ]
+};
