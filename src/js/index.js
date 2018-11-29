0ed13fab7324516ebaab99135bd78a18606e44d9
alex-w-dev <yampi.ru@gmail.com>
2018-11-29 15:25:51 +0500

    add phaser 'hello world'

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,49 @@
-console.log('all right');
+import Phaser from 'phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.setBaseURL('http://localhost:8080');
+
+  this.load.image('sky', 'img/spaceArt/png/Background/starBackground.png');
+  this.load.image('logo', 'img/spaceArt/png/player.png');
+  this.load.image('red', 'img/spaceArt/preview.jpg');
+}
+
+function create ()
+{
+  this.add.image(400, 300, 'sky');
+
+  const particles = this.add.particles('red');
+
+  const emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD'
+  });
+
+  const logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
